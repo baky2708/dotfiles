78967dd85d85053dbd992310e7a4260ea2dcc54b
baky2708 <flavioisaque123@gmail.com>
2022-02-14 23:53:13 -0300

    feat(nvim): add lsp eslint

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,1 +1,13 @@
-require('lspconfig').tsserver.setup{}
+local lspconfig = require('lspconfig')
+lspconfig.tsserver.setup{}
+lspconfig.eslint.setup{}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+     virtual_text = {
+       spacing = 4,
+     },
+    -- delay update diagnostics
+    update_in_insert = true,
+  }
+)
